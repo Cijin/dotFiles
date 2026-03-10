3a22171ea97ccc9134afecccfff6e56ea72398a2
seagin <cijin@seagin.me>
2026-03-10 13:42:04 +0700

    find directory

--- nvim/lua/seagin/set.lua
+++ nvim/lua/seagin/set.lua
@@ -66,3 +66,18 @@ vim.keymap.set("n", "<leader>pf", function()
         vim.cmd("copen")
     end)
 end)
+
+-- find directory
+vim.keymap.set("n", "<leader>pd", function()
+    vim.ui.input({ prompt = "find: " }, function(input)
+        if not input then return end
+        local results = vim.fn.systemlist("find " .. vim.fn.getcwd() .. " -type d -not -path '*/.git/*' -name " .. vim.fn.shellescape(input))
+        if vim.v.shell_error ~= 0 then return end
+        vim.fn.setqflist({}, "r", {
+            title = "find: " .. input,
+            lines = results,
+            efm = "%f"
+        })
+        vim.cmd("copen")
+    end)
+end)
