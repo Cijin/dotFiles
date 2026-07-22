e86509488e582fd451bf7549adb6a1feaecb8f4b
seagin <cijin@seagin.me>
2026-07-22 18:02:46 +0700

    remove hypr config

--- nvim/lua/seagin/packer.lua
+++ nvim/lua/seagin/packer.lua
@@ -5,7 +5,7 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   -- View stuff
-  use 'oxidescheme/nvim'
+  use 'vague-theme/vague.nvim'
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use 'folke/todo-comments.nvim'
 
